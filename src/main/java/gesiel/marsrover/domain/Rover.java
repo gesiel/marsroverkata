3673cd79a509b02dbadda516b0d4fd1efca5d39f
Gesiel <gesiel.sin@gmail.com>
2017-11-19 16:47:20 -0200

    WIP: Rover rotates to right

--- src/main/java/gesiel/marsrover/domain/Rover.java
+++ src/main/java/gesiel/marsrover/domain/Rover.java
@@ -4,13 +4,20 @@ import gesiel.marsrover.presentation.controllers.RoverRestController;
 
 public class Rover {
 
-    public void move(String command) {
+    private String direction = "N";
 
+    public void move(String command) {
+        command.chars().forEach(value -> {
+            if (direction.equals("N")) direction = "E";
+            else if (direction.equals("E")) direction = "S";
+            else if (direction.equals("S")) direction = "W";
+            else direction = "N";
+        });
     }
 
     public void position(RoverPositionOutputBoundary outputBoundary) {
         outputBoundary.x(0);
         outputBoundary.y(0);
-        outputBoundary.direction("N");
+        outputBoundary.direction(direction);
     }
 }
--- src/test/java/gesiel/marsrover/domain/RoverTest.java
+++ src/test/java/gesiel/marsrover/domain/RoverTest.java
@@ -1,11 +1,15 @@
 package gesiel.marsrover.domain;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class RoverTest {
 
     private Rover rover;
@@ -21,6 +25,21 @@ public class RoverTest {
     public void shouldInitializePointingToNorth() {
         rover.position(outputBoundary);
         assertPosition(0, 0, "N");
+    }
+
+    @Test
+    @Parameters({
+        "R, 0, 0, E",
+        "RR, 0, 0, S",
+        "RRR, 0, 0, W",
+        "RRRR, 0, 0, N",
+        "RRRRRRRR, 0, 0, N",
+        "RRRRRRRRRR, 0, 0, S",
+    })
+    public void shouldRotateToRight(String command, int expectedX, int expectedY, String expectedDirection) {
+        rover.move(command);
+        rover.position(outputBoundary);
+        assertPosition(expectedX, expectedY, expectedDirection);
     }
 
     private void assertPosition(int expectedX, int expectedY, String expectedDirection) {
